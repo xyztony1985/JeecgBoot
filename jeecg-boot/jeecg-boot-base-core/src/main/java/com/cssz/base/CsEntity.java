a9c897b59f3d6f97c13569c23ea8e1a0ce588885
xyztony1985 <xyztony1985@163.com>
2026-07-21 08:24:20 +0800

    feat(base-core): add CsEntity and UserUtil utility class
    add extended entity base class CsEntity with delete related fields and owner org code, add UserUtil for getting current login user info

---
+++ jeecg-boot/jeecg-boot-base-core/src/main/java/com/cssz/base/CsEntity.java
@@ -0,0 +1,32 @@
+package com.cssz.base;
+
+import java.util.Date;
+
+import org.jeecg.common.system.base.entity.JeecgEntity;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+/**
+ * @Description: 实体基类，扩展自 JeecgEntity，增加删除人和所有者部门编码等公共字段
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+public class CsEntity extends JeecgEntity {
+    @Schema(description = "删除人")
+    private String deleteBy;
+
+    @Schema(description = "删除时间")
+    @JsonFormat(timezone = "GMT+8", pattern = "yyyy-MM-dd HH:mm:ss")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date deleteTime;
+
+    @Schema(description = "所有者部门编码")
+    private String ownerOrgCode;
+}
